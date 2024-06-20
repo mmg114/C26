8214c2c60719430d4e999f768b216df5e04a6031
Mauricio Munoz Guzman <mauricio.m@moabits.com>
2024-06-19 19:42:31 -0500

    clase 2

--- src/main/java/trabajos/Trabajo1.java
+++ src/main/java/trabajos/Trabajo1.java
@@ -2,4 +2,7 @@ package trabajos;
 
 public class Trabajo1 {
     //Hacer una calculadora
+    public static void main(String[] args) {
+
+    }
 }
---
+++ src/main/java/trabajos/clases1/Clase1.java
@@ -0,0 +1,39 @@
+package trabajos.clases1;
+
+import java.util.List;
+import java.util.Map;
+
+public class Clase1 {
+    public static void main(String[] args) {
+
+        //Variables primitivas
+        int  numero = 12;
+        long numerosLargos=37828937;
+        float decimales;
+        double decimalesLargos;
+        boolean esBooleano= true;
+        char unCaracter ='m';
+        short numerosPequenos= 120;
+        //wrapper
+        Integer  numeroV2 = 12;
+        Long numerosLargosV2 = 37828937L;
+        Float decimalesV2;
+        Double decimalesLargosV2;
+        Boolean esBooleanoV2= false;
+        Character unCaracterV2 ='m';
+        String palabras="Hola mundo.";
+
+        //EJEMPLO DE NULL 0 >
+        Integer papelHigienico;
+        papelHigienico=10;
+        papelHigienico=0;
+        papelHigienico=null;
+
+        var nuevaPalabra=papelHigienico.toString();
+        var miPalabra = 12;
+
+        List<Map<String, List<String>>> variableCompleja;
+        Object a;
+
+    }
+}
