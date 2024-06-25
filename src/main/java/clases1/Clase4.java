6f5b0792e7bd9bc4a714bcd7830fef1f6c796023
Mauricio Munoz Guzman <mauricio.m@moabits.com>
2024-06-24 19:26:02 -0500

    clase 5

---
+++ src/main/java/clases1/Clase4.java
@@ -0,0 +1,51 @@
+package clases1;
+
+import java.util.Scanner;
+
+public class Clase4 {
+
+    public static void main(String[] args) {
+
+//        Integer edad = 55;
+//
+//        if (edad==null) {
+//            System.out.println("la edad es null");
+//        } else {
+//            System.out.println("Es menor de edad");
+//            if (edad >100) {
+//                System.out.println("Esta persona es muy vieja");
+//            }
+//        }
+        Scanner scanner = new Scanner(System.in);
+        try {
+
+            System.out.println("Ingrese un numero");
+
+            int numero= scanner.nextInt();
+
+
+            switch (numero) {
+                case 1:
+                    System.out.println("El número es uno");
+
+                    break;
+                case 2:
+                    System.out.println("El número es dos");
+                    break;
+                case 3:
+                    System.out.println("El número es tres");
+                    break;
+                default:
+                    System.out.println("El número es mayor que tres o menos que uno");
+            }
+        }catch (RuntimeException r){
+            System.out.println(r);
+        }finally {
+            scanner.close();
+        }
+
+
+
+
+    }
+}
---
+++ src/main/java/trabajos/Trabajo3.java
@@ -0,0 +1,5 @@
+package trabajos;
+//   dados 3 numeros digitables decir cual es el mayor.
+//   hacer un algoritmo que detecte en que mes estamos segun un numero del 1 al 12
+public class Trabajo3 {
+}
